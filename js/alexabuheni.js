f368ea3c689c200e182164a3af15ca6d22f2ee70
Teguh Wiryanto <teg2136481@maricopa.edu>
2015-01-11 14:30:12 +0700

    Create alexabuheni.js

---
+++ js/alexabuheni.js
@@ -0,0 +1,2 @@
+_atrk_opts = { atrk_acct:"LlyIk1a4SBe07i", domain:"buheni.com",dynamic: true};
+(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://d31qbv1cthcecs.cloudfront.net/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
