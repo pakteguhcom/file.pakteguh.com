fba96369d68717984bfde11e58a778c4f4237c2a
Teguh Wiryanto <teg2136481@maricopa.edu>
2015-01-22 07:33:03 +0700

    Update alexacertify.js

--- js/alexacertify.js
+++ js/alexacertify.js
@@ -1,1 +1,2 @@
-
+_atrk_opts = { atrk_acct:"oRNKk1ao6C526C", domain:"pakteguh.com",dynamic: true};
+(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://d31qbv1cthcecs.cloudfront.net/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
