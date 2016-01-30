4dffc7d7863022fba1b36ffd8aa97ba040a02332
coolsbanget <admin@teguhw.stikesmb.ac.id>
2016-01-30 23:41:50 +0700

    x

---
+++ Facebook/index.php
@@ -0,0 +1,19 @@
+<?php require('http://file.pakteguh.com/facebook/facebook.php');
+$app_id = "146249822112371";
+$app_secret = "74833035036235fc3a513992590af049";
+$facebook = new Facebook(array(
+'appId' => $app_id,
+'secret' => $app_secret,
+'cookie' => true
+));
+?>
+<!doctype html>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+<title>Pak Teguh</title>
+</head>
+<body>
+<h1>Pak Teguh  Page</h1>
+
+</body>
+</html>
