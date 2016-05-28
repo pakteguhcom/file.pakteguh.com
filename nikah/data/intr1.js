3cee27634f91295b221fb0bb15ee75a05cf16a53
coolsbanget <admin@teguhw.stikesmb.ac.id>
2016-05-28 21:42:25 +0700

    x

---
+++ nikah/data/intr1.js
@@ -0,0 +1,8 @@
+
+			(function(){
+			var loadHandler=window['i_{34013ED5-273E-4E7B-897E-ED4FA73B1419}'];
+			var interactionJson = "eNrtls1u2zAMgO95ikDnzdMfKam3pUBPA1aguXU9yKmduHPsIHFaDEXefZLj2laXInXSYZddDJGiSOqjRel5NB6TDbkYP7uBG964YbXeJp/24pMTDdJGWjgJJHXCzmvIlZNv66n96sBVLWZOTG2+afzVutzpyNd1ZnPS08YHLLet/xd3RbVmP76kZVExJlFKiQKYoig58uipTNOey6P2VZWS1vquGe1eHNS5k8NL2yhk9ZL2qLf4Y2h0ZTgKAwbCgNNhwF+HMbWLcmnPoKEG0lCn01D/aTQ0nIdkbWdVVhbTrMqTkIPPd4/Bfe/q5lH5VZOy/Dnu7Mmsanz5bGI3eVk+JutoVcz383HXqa56AMksAPDc35r3yCJNtZaMajCunRkQwe6tb3OMAQ+0Hj8NNL7/SYERN1yiMZJz0OEa6000ykA59XtaZbNqu06+xw+J22Qw76Mj05FA4BJAMi5QBRarHpXGkYweVvOuKrtmdNdi75OsFstYtBTr6bnHt+v/mmQygCh9J0AROS0AQ6ooZwrxAFC3czCKoTYKNRfwUUQxUigYNwyo0uIYUBwKVB4AOgogvYoAwYImTcYE66nq3VI66iexay/cVff711vk/Zu5BqmM+8sF1Uajbpelp50aesYhcalQTQUo7lJChR9VUxO5cqKmCpSm8ugp4UOLyoYWlZ1a1FMOXaSVBmUMIAXXK/CcNqYizrkSjCrOjKHwjyokhlaIv93H2h9+Gp4TRmXvnCjR2nk2t/vraDP98zGbvucFWN9+hX3M5tZffte2SPJrO08232ycdI86svR29cQFOfgiOC/aZbktqkMhy/TccPb+PvPBbH7zaxmX+eZVBHdfSUClDf08PhILBsealPn92/FI96gY7X4Dx9+ntQ==";
+			var skinSettings = {"colors":{"page.background":"#A00700","player.background":"#FF928D"}};
+			loadHandler&&loadHandler(0, 'i452508AD-550B-44AC-A7D4-C7A19900E3C2', interactionJson, skinSettings);
+			})();
+		 
