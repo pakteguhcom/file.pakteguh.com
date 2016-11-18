749b930bfd023aa46ebc7269d95036456a8639cd
Teguh Wiryanto <teg2136481@maricopa.edu>
2016-11-18 20:32:22 +0700

    Create dropbear.sh

---
+++ dropbear.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+sudo rpm -ivh epel-release-6-8.noarch.rpm
+sudo yum update && sudo yum install dropbear
+sudo chkconfig dropbear on
+clear
+sudo service dropbear start
+echo "www.pakteguh.com"
